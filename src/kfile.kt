9c995f4ecc9f37078a06ae359a2d794978ab4c16
zerogon <safoma@naver.com>
2019-12-30 17:19:30 +0900

    f_c

---
+++ src/kfile.kt
@@ -0,0 +1,17 @@
+fun main(argv: Array<String>) {
+	println("Hello Kotlin")
+	var a = 1
+	val b =1
+	
+}
+
+fun sum(a: Int, b: Int) : Int {
+	return a+b
+}
+
+fun sum2(a:Int, b:Int) : Int = a+b
+
+fun max(a:Int, b:Int) : Int = if (a>b) a else b
+
+
+
